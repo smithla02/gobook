b5a70341424db241aca4228bc2da62c584cca8e6
Luis Smith <smithla02@gmail.com>
2017-09-17 00:54:54 -0400

    Chapter 1 edits

---
+++ ch1/fetchall/main.go
@@ -0,0 +1,72 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 17.
+//!+
+
+// Fetchall fetches URLs in parallel and reports their times and sizes.
+package main
+
+import (
+	"bufio"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	ch := make(chan string)
+	var urls []string
+	csvFile, _ := os.Open("top-400.csv")
+	reader := csv.NewReader(bufio.NewReader(csvFile))
+	for {
+		line, err := reader.Read()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			log.Fatal(err)
+		} else {
+			currURL := line[1]
+			if !strings.HasPrefix(currURL, "http://www.") {
+				currURL = "http://www." + currURL
+			}
+			urls = append(urls, currURL)
+		}
+	}
+
+	for _, url := range urls {
+		go fetch(url, ch) // start a goroutine
+		//fmt.Println(url)
+	}
+	for range urls {
+		fmt.Println(<-ch) // receive from channel ch
+	}
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+}
+
+func fetch(url string, ch chan<- string) {
+	start := time.Now()
+	resp, err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprint(err) // send to channel ch
+		return
+	}
+
+	nbytes, err := io.Copy(ioutil.Discard, resp.Body)
+	resp.Body.Close() // don't leak resources
+	if err != nil {
+		ch <- fmt.Sprintf("while reading %s: %v", url, err)
+		return
+	}
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%.2fs  %7d  %s", secs, nbytes, url)
+}
+
+//!-
